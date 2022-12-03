13fa8bc4126c39a58f3b38ba359d0df1838bcdb8
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-03 09:17:30 -0500

    added basic flask app server running

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+def create_app(test_config=None):
+    # set up app oncfig
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(SECRET_KEY = 'super_secret_key')
+    return app
