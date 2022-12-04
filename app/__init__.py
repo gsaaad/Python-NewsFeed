6d0bb84ed38a6904aeacbd92497b606e4c51a64c
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-12-04 15:17:03 -0500

    addition of route to flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+# activate venv
+# .\venv\Scripts\activate
 
 def create_app(test_config=None):
     # set up app oncfig
+    
+    
     app = Flask(__name__, static_url_path='/')
     app.url_map.strict_slashes = False
     app.config.from_mapping(SECRET_KEY = 'super_secret_key')
+    
+    # add routes
+    @app.route('/hello')
+    def hello():
+        return 'hello world'
     return app
