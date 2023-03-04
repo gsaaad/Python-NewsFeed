d956c82c153bdb662999632277d24a4b902c3531
gsaaad <67985748+gsaaad@users.noreply.github.com>
2023-03-04 16:25:02 -0500

    addition of login and logout functions and rerouting user to dashboard when user attempts to reach login page

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 import sys
 from app.db import get_db
@@ -20,8 +20,9 @@ def signup():
             email = data['email'],
             password = data['password']
         )
-        # save in database
+        # ADD user 
         db.add(newUser)
+        # commit/save database changed
         db.commit()
         print(message)
         
@@ -33,9 +34,49 @@ def signup():
         # insert failed, so send error to front end
         message = 'Registration failed.. Try again later'
         print(message)
+        # if there was an error, rollback / cancel pending process
         db.rollback()
         return jsonify(message), 500
     print(newUser)
-    print(sys.exe_info()[0])
-
+    # print(sys.exe_info()[0])
+    
+    # adding session of user login
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
+    
+    
+    # for front end, we want to return a more meaningful response. JSON format
     return jsonify(id=newUser.id)
+
+@bp.route("/users/logout", methods =['POST'])
+def logout():
+    # remove session variables
+    session.clear()
+    return '', 204
+
+
+@bp.route("/users/login", methods=['POST'])
+def login():
+    data = request.get_json()
+    db = get_db()
+    
+    try:
+        user = db.query(User).filter(User.email == data['email']).one()
+        
+        
+    except:
+        print(sys.exc_info()[0])
+        
+        return jsonify(message = 'Incorrect credentials'), 400
+    if user.verify_password(data['password']) ==False:
+            return jsonify(message = 'Incorrect credentials'), 400
+    
+    session.clear()
+    session['user_id'] = user.id
+    session['loggedIn'] = True
+    
+    return jsonify(id = user.id)
+    
+    
+    
